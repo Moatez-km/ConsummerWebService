c53c89bec622b9abc11e2560c8fb1330898c3884
moatez kamoun <moatez.kamounn@gmail.com>
2021-05-24 01:33:47 +0100

    response consummer

--- src/main/java/de/tekup/loan/invoke/controller/CheckWhiteTestController.java
+++ src/main/java/de/tekup/loan/invoke/controller/CheckWhiteTestController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import de.tekup.DS.soap.api.consume.whiteTest.StudentRequest;
+import de.tekup.DS.soap.api.consume.whiteTest.WhiteTestResponse;
 import de.tekup.loan.invoke.client.SoapClient;
 
 @Controller
@@ -21,6 +23,13 @@ public class CheckWhiteTestController {
 		return "request";
 	}
 
+	@PostMapping("/check/student")
+	public String WhiteTests(@ModelAttribute("request") StudentRequest request,
+			Model model) {
+		WhiteTestResponse response = client.getWhiteTestDetails(request);
+		model.addAttribute("response",response);
+		return "response";
+	}
 	
 
 }
